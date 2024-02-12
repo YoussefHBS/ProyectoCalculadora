21caa34b715e381fd3cd8f8c3b9fd9690cbd1a35
youssef <yhab0112@g.ecudaand.es>
2024-02-12 13:01:13 +0100

    Commit Inicial

---
+++ Calculadora.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+              int opcion;
+                do {
+                    
+                    System.out.println("Seleccione una operación:");
+                    System.out.println("1. Suma");
+                    System.out.println("0. Salir");
+                    opcion = scanner.nextInt();
+                    System.out.print("Dime el primer numero: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Dime el segundo numero: ");
+        int num2 = scanner.nextInt();
+        pruebaCalculadora calculadora = new pruebaCalculadora(num1, num2); 
+        int resultado;
+                    
+                    switch (opcion) {
+                        case 1:
+                            resultado = calculadora.suma();
+                            System.out.println("El resultado de la suma es: " + resultado);
+                            break;
+
+                        default:
+                            System.out.println("Opción inválida. Intente nuevamente.");
+                            break;
+                    }
+                } while (opcion != 0);
+            }
+        }
+
---
+++ pruebaCalculadora.java
@@ -0,0 +1,15 @@
+public class pruebaCalculadora {
+    private int num1;
+    private int num2;
+    
+    public pruebaCalculadora(int num1, int num2) {
+
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+    public int suma() {
+        return num1 + num2;
+    }
+
+}
